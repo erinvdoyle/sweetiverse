cf0c6ef6355e1a9a0ade1bcb06ab9913bfee2eb0
erinvdoyle <erin.v.doyle@gmail.com>
2025-06-06 12:31:58 +0100

    update sweets/admin.py to register the models added in the previous commit

--- sweets/admin.py
+++ sweets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Sweet, Type, Category
 
 # Register your models here.
+
+admin.site.register(Sweet)
+admin.site.register(Type)
+admin.site.register(Category)
