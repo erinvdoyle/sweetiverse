5b352a3860837746a71e17680db7ac3e5af7f112
erinvdoyle <erin.v.doyle@gmail.com>
2025-06-12 07:54:26 +0100

    update functionality of shopping bag, add sweetiverse success messages,create and style bag product info and order summary

--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<sweet_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<sweet_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<sweet_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from sweets.models import Sweet
+from django.contrib import messages
 
 
 # Create your views here.
@@ -26,3 +27,36 @@ def add_to_bag(request, sweet_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, sweet_id):
+    """
+    Adjust the quantity of the specified sweet to the specified amount.
+    """
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[sweet_id] = quantity
+        messages.success(request, 'Sweet quantity updated!')
+    else:
+        bag.pop(sweet_id, None)
+        messages.success(request, 'Sweet removed from your bag.')
+
+    request.session['bag'] = bag
+    return redirect('view_bag')
+
+
+def remove_from_bag(request, sweet_id):
+    """
+    Remove the item from the shopping bag.
+    """
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(sweet_id, None)
+        request.session['bag'] = bag
+        messages.success(request, 'Sweeti removed from your bag.')
+        return redirect('view_bag')
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return redirect('view_bag')
