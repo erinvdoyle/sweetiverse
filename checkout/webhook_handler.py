137e0508e42910f4427e57cde019da6213db659b
erinvdoyle <erin.v.doyle@gmail.com>
2025-06-13 07:28:37 +0100

    add webhooks.py an

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+
+class StripeWH_Handler:
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """Generic event handler."""
+        return HttpResponse(content=f'Unhandled webhook received: {event["type"]}', status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """Handle successful payment."""
+        return HttpResponse(content='PaymentIntent succeeded webhook received.', status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """Handle failed payment."""
+        return HttpResponse(content='PaymentIntent failed webhook received.', status=200)
---
+++ checkout/webhooks.py
@@ -0,0 +1,39 @@
+import stripe
+import json
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from .webhook_handler import StripeWH_Handler
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    payload = request.body
+    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(payload, sig_header, wh_secret)
+    except ValueError as e:
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=f"Unhandled error: {e}", status=400)
+
+    handler = StripeWH_Handler(request)
+    event_type = event['type']
+
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    event_handler = event_map.get(event_type, handler.handle_event)
+    response = event_handler(event)
+
+    return response
