0823b7ecc21fbfedc531bbbb7535f13fa15f44ab
erinvdoyle <erin.v.doyle@gmail.com>
2025-06-20 22:52:52 +0100

    add privacy_policy template and link from footer

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('faq/', views.faq, name='faq'),
+    path("privacy/", TemplateView.as_view(template_name="home/privacy.html"), name="privacy"),
 ]
